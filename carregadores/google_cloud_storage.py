6c9336509a7948b5b982ffe246a875dd825099d2
bazico <sejabazico@gmail.com>
2023-01-30 16:06:31 -0300

    docs: Adição de definição de tipo na definição dos parâmetros de função

--- carregadores/google_cloud_storage.py
+++ carregadores/google_cloud_storage.py
@@ -1,11 +1,14 @@
 import os
 
 from google.cloud import storage
+from pathlib import Path
+
+from tipos import IO
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../arquivos/credenciais/chave_datalake.json'
 
 
-def subir_para_o_bucket(caminho_do_arquivo, id_projeto, nome_do_bucket):
+def subir_para_o_bucket(caminho_do_arquivo: Path, id_projeto: str, nome_do_bucket: str) -> IO:
     cliente_storage = storage.Client(project=id_projeto)
     bucket = cliente_storage.bucket(nome_do_bucket)
     blob = bucket.blob(caminho_do_arquivo)
