3f8300e221990551de698816ceb6dd5a3a3a8071
gustavomello9600 <gustavomelloenciv@gmail.com>
2023-01-31 00:45:25 -0300

    refactor: Melhoria na estrutura de pastas e coesão entre nomes e implementações de funções extratoras e transformadoras
    * Arquivos temporários agora vão para a pasta "cache" no diretório raiz
    * Todas as funções contam com a flag "salvar_json" ou "salvar_parquet"

--- extratores/bling.py
+++ extratores/bling.py
@@ -1,4 +1,5 @@
 from itertools import count
+from pathlib import Path
 
 import requests
 import json
@@ -7,9 +8,10 @@ from tipos import List, Pedido, Produto
 
 
 APIKEY = "a3e36e25a52e280465de4b060619486ff2cd0292395bfdac81482c015293b25b250c3104"
+CAMINHO_PARA_ARQUIVOS_DE_CACHE = Path(__file__).parent.parent / "cache"
 
 
-def todos_os_pedidos(apikey: str = APIKEY) -> List[Pedido]:
+def todos_os_pedidos(apikey: str = APIKEY, salvar_json=True) -> List[Pedido]:
     pedidos = []
     for i in count(1, step=1):
         resposta = requests.get(f"https://bling.com.br/Api/v2/pedidos/page={i}/json/",
@@ -20,6 +22,12 @@ def todos_os_pedidos(apikey: str = APIKEY) -> List[Pedido]:
                 break
         else:
             pedidos += resposta["retorno"]["pedidos"]
+
+    if salvar_json:
+        with open(CAMINHO_PARA_ARQUIVOS_DE_CACHE / "todos_os_pedidos.json", "w") as arquivo:
+            json.dump(pedidos, arquivo)
+        print('Arquivo todos_os_pedidos.json salvo.')
+
     return pedidos
 
 
@@ -34,10 +42,12 @@ def todos_os_produtos(apikey: str = APIKEY, salvar_json=True) -> List[Produto]:
                 break
         else:
             produtos += resposta['retorno']['produtos']
-    if salvar_json == True:
-        with open('../arquivos/produtos.json', 'w') as arquivo:
+
+    if salvar_json:
+        with open(CAMINHO_PARA_ARQUIVOS_DE_CACHE / "produtos.json", "w") as arquivo:
             json.dump(produtos, arquivo)
-        print('Arquivo .json salvo.')
+        print('Arquivo produtos.json salvo.')
+
     return produtos
 
 
