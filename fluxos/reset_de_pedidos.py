90aa968bb6770484fd2568d1aeaa4c3da2d87699
gustavomello9600 <gustavomelloenciv@gmail.com>
2023-02-02 16:24:35 -0300

    feature: Adicionado gatilho de cloud function que sobrescreve tabela do bigquery com o parquet do bucket layer_2

--- fluxos/reset_de_pedidos.py
+++ fluxos/reset_de_pedidos.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import requests
+
 import extratores
 import transformadores
 import carregadores
@@ -10,6 +12,37 @@ ID_PROJETO = 'datalake-375813'
 NOME_BUCKET_LAYER_RAW = 'bzco_layer_raw'
 NOME_BUCKET_LAYER_2 = 'bzco_layer_2'
 CAMINHO_PARA_ARQUIVOS_DE_CACHE = Path(__file__).parent.parent / "cache"
+IDENTITY_TOKEN = ("eyJhbGciOiJSUzI1NiIsImtpZCI6IjI3NDA1MmEyYjY0NDg3NDU3NjRlNzJjMzU5MDk3"
+                  "MWQ5MGNmYjU4NWEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY"
+                  "29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zM" 
+                  "m42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4M"
+                  "DU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlc"
+                  "mNvbnRlbnQuY29tIiwic3ViIjoiMTA3NTc3ODc3NDg0MTMzMTYyMjYwIiwiZW1haWwiO"
+                  "iJkYWRvcy5iYXppY29AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X"
+                  "2hhc2giOiJ4aXZjVFVzTHQtUzVZdFZFZ0d3b2JnIiwiaWF0IjoxNjc1MzYxNzU1LCJle"
+                  "HAiOjE2NzUzNjUzNTUsImp0aSI6ImI1ODAyZDhhOTlkNWNlZTUzNjliZjdhNzBmMDBmN"
+                  "GY3ZGQ1ZDYzMjkifQ.uuUQRiTxLaJYAmf5vbWkDeVC92BlTUlN9BdMsWSGdMYLKXez6p"
+                  "P0_PQA0qz7y0LASJjpIL4zeviKSdYsIvvcbLYTujre_eco45CHUXLnJdfokWRm5N-39c"
+                  "nWMOJ2jmthDfAOg5y9uDhq9R5IKnJpfxOUsw8G8RZLeOwC3If9yAHTeq0fr5zLBoiXp-"
+                  "HwneTemrJoZ_NvJ4okgMyDZBQq9eVcvVyiJaqED9SSfg9h3j4zAawjBLcVGkLB67O6FO"
+                  "65eY6ztytsW-lw8BoTy81DvMD9Jmgt0PQgAn9CFjEPnDNDiweeKuderkm9i8_yE2Jf3A"
+                  "Dw1mnmwvmx4URrWOk80A")
+
+
+def gatilho_da_cloud_function():
+    url = "https://sobrescrever-tabela-de-pedidos-znz5wp6mbq-uc.a.run.app"
+    headers = {"Authorization": f"bearer {IDENTITY_TOKEN}",
+               "Content-Type": "application/json"}
+    data = {"acionador": "qualquer"}
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        print("Request succeeded with status code: ", response.status_code)
+        print("Response content: ", response.content)
+    else:
+        print("Request failed with status code: ", response.status_code)
+        print("Response content: ", response.content)
 
 
 def reset_de_pedidos() -> IO:
@@ -31,6 +64,7 @@ def reset_de_pedidos() -> IO:
         id_projeto=ID_PROJETO,
         nome_do_bucket=NOME_BUCKET_LAYER_2,
         nome_do_blob="pedidos.parquet")
+    gatilho_da_cloud_function()
 
 
 if __name__ == '__main__':
